2f9f8702efe6f18faa25e839f2421a37c9c8718f
ikramhasib007 <ikramhasib007@gmail.com>
2018-05-10 10:35:03 +0600

    prepare backend

---
+++ app/Http/Controllers/Api/PostsController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return response($posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = new Post;
+        $request->validate([
+            'title' => 'required|unique:posts|max:255',
+            'category_id' => 'required|integer',
+            'body' => 'required'
+        ]);
+        $post->title = $request->title;
+        $post->category_id = $request->category_id;
+        $post->slug = str_slug($request->title);
+        $post->body = $request->body;
+        
+        $post->save();
+        return response($post);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|max:255|unique:posts,title,'.$id.'id',
+            'category_id' => 'required|integer',
+            'body' => 'required'
+        ]);
+            
+        $post = Post::find($id);
+        
+        $post->title = $request->title;
+        $post->category_id = $request->category_id;
+        $post->slug = str_slug($request->title);
+        $post->body = $request->body;
+        
+        $post->save();
+
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+        $post->delete();
+        return $post;
+    }
+}
